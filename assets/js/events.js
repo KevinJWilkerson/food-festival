b86e8fbc1db290f799dc0a53eeecc5e0bd089c17
Kevin <kevin.j.wilkerson@gmail.com>
2022-03-16 22:06:06 -0500

    bundled code and optimized images with webpack config

---
+++ assets/js/events.js
@@ -0,0 +1,15 @@
+require("bootstrap");
+const createEl = require("./domMethods");
+const createLoremIpsum = require("./helpers");
+
+$(document).ready(function () {
+  const currentEvent = JSON.parse(localStorage.getItem("currentEvent")) || {
+    title: "Title Placeholder",
+    subtitle: "",
+    description: "",
+  };
+
+  const pageEl = document.querySelector("#page");
+
+  pageEl.appendChild(containerEl);
+});
